ee5981d4071b3d70efb2edb73c8bd71b70936cb3
Joseph Sylvanovich <sylvanovich@google.com>
2018-12-30 16:01:58 -0500

    Initial Commit

---
+++ Prework_SylvanovichTests/Prework_SylvanovichTests.swift
@@ -0,0 +1,34 @@
+//
+//  Prework_SylvanovichTests.swift
+//  Prework_SylvanovichTests
+//
+//  Created by Joseph Sylvanovich on 12/30/18.
+//  Copyright © 2018 Joseph.Sylvanovich. All rights reserved.
+//
+
+import XCTest
+@testable import Prework_Sylvanovich
+
+class Prework_SylvanovichTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
